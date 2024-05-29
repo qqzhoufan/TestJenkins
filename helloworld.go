99408258115b9b207d6492fe4b1014f85b1b9581
qqzhoufan <237919060@qq.com>
2024-05-29 09:33:34 +0000

    删除！ --zhou

--- helloworld.go
+++ helloworld.go
@@ -6,9 +6,5 @@ func main() {
 	for {
 		fmt.Println("Hello World")
 		fmt.Println("爽儿子天下无敌！")
-		fmt.Println("陈宇航大胖儿子！")
-		fmt.Println("陈宇航大胖儿子！")
-		fmt.Println("陈宇航大胖儿子！")
-		fmt.Println("陈宇航我是你爹！")
 	}
 }
